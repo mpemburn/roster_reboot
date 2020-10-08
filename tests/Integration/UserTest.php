c84b24e62d37892f7ad7976165494a73ae0e62df
Mark Pemburn <mark@pemburn.com>
2020-10-08 11:11:27 -0400

    Add Role and Permissions tests.

--- tests/Integration/UserTest.php
+++ tests/Integration/UserTest.php
@@ -4,11 +4,16 @@ namespace Tests\Integration;
 
 use App\Models\User;
 use Illuminate\Support\Facades\Artisan;
+use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
+use Spatie\Permission\PermissionRegistrar;
 use Tests\TestCase;
 
 class UserTest extends TestCase
 {
+    protected $writerRole;
+    protected $adminRole;
+    protected $superRole;
 
     public function setUp(): void
     {
@@ -20,12 +25,44 @@ class UserTest extends TestCase
     public function testRoleNotFound(): void
     {
         $user = User::factory()->make();
-        $this->assertFalse($user->hasRole('chicken'));
+        self::assertFalse($user->hasRole('chicken'));
 
         $role = Role::create(['name' => 'turkey']);
         $user->assignRole($role);
 
-        $this->assertFalse($user->hasRole('chicken'));
+        self::assertFalse($user->hasRole('chicken'));
+    }
+
+    public function testUserRoleFound(): void
+    {
+        $user = User::factory()->create([
+            'name' => 'Example Super-Admin User',
+            'email' => 'superadmin@example.com',
+        ]);
+
+        $this->superRole  = Role::create(['name' => 'super-admin']);
+        $user->assignRole($this->superRole);
+
+        self::assertTrue($user->hasRole('super-admin'));
+    }
+
+    public function testUserRoleWithPermissionsFound(): void
+    {
+        $user = User::factory()->create([
+            'name' => 'Example User',
+            'email' => 'test@example.com',
+        ]);
+
+        Permission::create(['name' => 'edit articles']);
+        Permission::create(['name' => 'delete articles']);
+
+        // create roles and assign existing permissions
+        $this->writerRole = Role::create(['name' => 'writer']);
+        $this->writerRole->givePermissionTo('edit articles');
+        $this->writerRole->givePermissionTo('delete articles');
+        $user->assignRole($this->writerRole);
+
+        self::assertTrue($user->hasRole('writer'));
     }
 
 }
