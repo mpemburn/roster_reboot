7daf44477f7049e176233daf7a7fcd0b6aae5e3f
Mark Pemburn <mark@pemburn.com>
2020-10-14 07:34:40 -0400

    Add Laravel Passport and configure.

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,11 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasFactory, HasRoles, Notifiable;
+    use HasFactory, HasRoles, HasApiTokens, Notifiable;
 
     /**
      * The attributes that are mass assignable.
