a6c12c12825efe79bdb9bd58f3ffbd2165ca25f2
Mark Pemburn <mark@pemburn.com>
2020-11-18 11:21:08 -0500

    Create Email model and factory.

---
+++ app/Models/Email.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Email extends Model
+{
+    use HasFactory;
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'email',
+        'is_primary',
+        'description'
+    ];
+
+    public function member(): BelongsTo
+    {
+        return $this->belongsTo(Member::class);
+    }
+}
