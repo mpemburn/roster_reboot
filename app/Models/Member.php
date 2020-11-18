aab187caf23ccaf85aef7631e1aa582ceb34e45c
Mark Pemburn <mark@pemburn.com>
2020-11-18 11:22:15 -0500

    Create relationship between Member and Email.

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Notifications\Notifiable;
 
@@ -53,5 +54,10 @@ class Member extends Model
     public function suffix(): HasOne
     {
         return $this->hasOne(Suffix::class);
+    }
+
+    public function emails(): HasMany
+    {
+        return $this->hasMany(Email::class);
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -49,5 +49,4 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-
 }
