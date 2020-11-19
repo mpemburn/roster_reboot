967f1f32bf65c3c09f8f8dd5aa417cebef96439e
Mark Pemburn <mark@pemburn.com>
2020-11-19 08:35:52 -0500

    Add further assertions to CreateUserTest.

--- tests/Integration/CreateUserTest.php
+++ tests/Integration/CreateUserTest.php
@@ -6,19 +6,27 @@ use App\Models\Email;
 use App\Models\Member;
 use Database\Seeders\DatabaseSeeder;
 use Database\Seeders\MemberSeeder;
+use Faker\Factory;
 use Tests\TestCase;
 
 class CreateUserTest extends TestCase
 {
+    protected $faker;
+
     public function setUp(): void
     {
         parent::setUp();
         // Do artisan migrate:refresh on test database
         $this->refreshDatabase();
         (new DatabaseSeeder())->call(MemberSeeder::class);
+
+        $this->faker = Factory::create();
     }
-    public function testUserHasCurrentMembership(): void
+
+    public function testUserCanBeCreated(): void
     {
+        $this->withoutExceptionHandling();
+
         $member = Member::factory()->create();
         $email = Email::factory()->create();
         $member->emails()->save($email);
@@ -26,15 +34,22 @@ class CreateUserTest extends TestCase
         // Test whether the email has been associated with the member
         self::assertTrue($member->emails()->where('email', $email->email)->exists());
 
-        // Attempt to register new user
-        $response = $this->post('/api/auth/signup', [
-            'name' => 'Mark Pemburn',
+        $password = $this->faker->password;
+        $attributes = [
+            'name' => $this->faker->firstName,
             'email' => $email->email,
-            'password' => 'zootSu1t',
-            'password_confirmation' => 'zootSu1t'
-        ]);
+            'password' => $password,
+            'password_confirmation' => $password
+        ];
+
+        // Attempt to register new user
+        $response = $this->post('/api/auth/signup', $attributes);
 
         $response->assertStatus(201);
 
+        $this->assertDatabaseHas('users', [
+            'name' => $attributes['name'],
+            'email' => $attributes['email']
+        ]);
     }
 }
