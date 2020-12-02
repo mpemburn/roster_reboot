bb90f6a351cae5b70a46f13226527f4c2e9c9b73
Mark Pemburn <mark@pemburn.com>
2020-12-02 07:05:27 -0500

    Update schema.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,10 +2,8 @@
 
 namespace Tests;
 
-use Database\Seeders\DatabaseSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
-use Illuminate\Support\Facades\Artisan;
 
 abstract class TestCase extends BaseTestCase
 {
